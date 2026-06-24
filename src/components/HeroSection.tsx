d7ceedaf52da18cc84d7b0728ac246e2bda4ca2a
Jawad Ashraf <jwadashraf772@gmail.com>
2026-06-24 15:14:49 +0500

    Fix layout wrapping of headline on mobile screens to look highly professional

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -46,13 +46,13 @@ export default function HeroSection() {
           >
             Get personalized guidance <br className="hidden sm:inline" />
             that has helped{" "}
-            <span className="relative inline-block text-red-600 px-1 font-black">
+            <span className="relative inline-block text-red-600 px-1 font-black whitespace-nowrap">
               1300+ students
               <span className="absolute left-0 bottom-1.5 w-full h-2 bg-red-100 rounded-full -z-10 transform -rotate-1" />
             </span>{" "}
             with <br className="hidden sm:inline" />
             <span className="text-purple-600">
-              admissions, scholarships, & careers
+              admissions, <br className="block sm:hidden" /> scholarships, & careers
             </span>
           </motion.h1>
 
